1f50daa112261648932f4cf16fdb98c8b8692d2e
journeysbeckon <165425181+journeysbeckon@users.noreply.github.com>
2024-03-31 05:47:52 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-// Fetch and display latest blog posts
+// Fetch blogs from Blogger API and display snippets dynamically
 window.onload = function() {
   fetch('https://www.googleapis.com/blogger/v3/blogs/1855713977688632827/posts?key=AIzaSyCBhhCcYNPI-p9TPeYcpdc7Pf2eLEwlZSg')
     .then(response => response.json())
@@ -8,15 +8,19 @@ window.onload = function() {
       blogPosts.forEach(post => {
         const postElement = document.createElement('div');
         postElement.classList.add('blog-post');
+        // Display only the first 100 characters of the content
+        const snippet = post.content.slice(0, 100) + '...';
         postElement.innerHTML = `
           <h3>${post.title}</h3>
-          <p>${post.content}</p>
+          <p>${snippet}</p>
           <a href="${post.url}" target="_blank">Read More</a>
         `;
         blogSection.appendChild(postElement);
       });
     })
     .catch(error => console.error('Error fetching blogs:', error));
+};
+
 
   // Fetch and display Instagram posts
   // Add your Instagram API fetch code here
