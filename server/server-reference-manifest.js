2daf62cbe48006245c3a7939226210cbeb474872
Journeys Beckon Github Action <journeysbeckon@gmail.com>
2024-04-20 12:42:44 +0000

    Update build artifacts

--- server/server-reference-manifest.js
+++ server/server-reference-manifest.js
@@ -1,1 +1,1 @@
-self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"w5TYwwTORbvgWYVD08/xTyJq1PcE5V0xsXcqI3pcwSU=\"}"
+self.__RSC_SERVER_MANIFEST="{\"node\":{},\"edge\":{},\"encryptionKey\":\"EOHOBgtIjld+3a4tv4xbQ/LzyVzHycY4VxrkO9kk5XY=\"}"
